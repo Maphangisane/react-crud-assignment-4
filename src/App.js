449f89c01ee318759e710e18b790334c79711049
Thabang H Maphangisane <102468265+Maphangisane@users.noreply.github.com>
2023-08-16 11:45:30 +0200

    commit changes before working on update button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,133 @@
+//Imports
+import { useState, useEffect } from "react"
+  //Components
 import Header from "./components/Header";
+import Members from "./components/Members";
+import AddMember from "./components/AddMember";
 
+
+// App Main Function
 function App() {
+
+// Defintions
+
+  // States
+      // form wil be dependent on this state
+      const [showAddTask, setShowAddTask] = useState(false)
+
+      // cut and paste sates in db.json and leave empty array
+      const [members, setMembers] = useState([])
+
+      // useEffect function to fetch data
+      useEffect(() => {
+          const getMembers = async () => {
+            const membersFromSever = await fetchMembers()
+            setMembers(membersFromSever)
+          }
+
+          getMembers()
+        }, [] // dependency array
+      )
+
+      // fetch members
+    const fetchMembers = async () => {
+      const res = await fetch('http://localhost:5000/members')
+      const data = await res.json()
+      
+      //console log to test
+      // console.log(data)
+      return data
+    }
+
+        // fetch member -getting a single member - add id and chamge to bCKTICKS
+        const fetchMember = async (id) => {
+          const res = await fetch(`http://localhost:5000/members/${id}`)
+          const data = await res.json()
+          
+          //console log to test
+          // console.log(data)
+          return data
+        }
+
+      // Add Task
+      const addMember = async (member) => {
+        // no need to create id cos dynamically created for us
+                const res =  await fetch('http://localhost:5000/members', 
+                {
+                  method: 'POST',
+                  headers: {
+                    'Content-type': 'application/json',
+                  },
+                  body: JSON.stringify(member),
+                })
+        
+                const data = await res.json()
+        
+                setMembers([...members, data])
+      }
+
+       // Delete member
+       const deleteMember = async (id) => {
+        await fetch(`http://localhost:5000/members/${id}`, 
+        {method: 'DELETE'})
+
+        setMembers(members.filter((member) => member.id !== id))
+      }             
+
+             
+      // Update member
+       const updateMember = async (id) => {
+          const memberToUpdate = await fetchMember(id)
+          const updatedMember = {...memberToUpdate,
+            name: memberToUpdate.name
+          }
+      
+       const res = await fetch(`http://localhost:5000/members/${id}`,
+          {
+              method: 'PUT',
+              headers: {
+                'Content-type': 'application/json'
+                },
+              body: JSON.stringify(updatedMember)
+          }
+        )
+      
+        const data = await res.json()
+      
+        // console.log(id)
+
+          setMembers(
+            members.map((member) => 
+            member.id === id ? 
+            { ...member, name: data.name} : member 
+           )
+          )
+        }
+
+
+// Returns
   return (
     <div className="container">
-      <Header/>
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+
+      {showAddTask && <AddMember onAdd= {addMember}/>}  
+
+      { members.length > 0 ? 
+      (<Members 
+          members={members} 
+          onDelete= {deleteMember} 
+          onToggle= {updateMember}
+        />
+      ) : 
+      ('No Members to show')
+      }
+
     </div>
   );
 }
 
+//Exports
 export default App;
---
+++ src/components/AddMember.js
@@ -0,0 +1,59 @@
+//Imports
+import { useState } from "react"
+
+// Definitions
+const AddMember = ( {onAdd} ) => {
+
+    // State Management
+    const [name, setName] = useState('')
+    const [job, setJob] = useState('')
+    // const [reminder, setReminder] = useState(false)
+    
+    // onsubmit function
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        // validation
+        if (!name) {
+            alert('Please add a Member')
+            return
+        }
+        // call on add method and pass in text day and reminder as objects
+
+        onAdd({name, job})
+
+        // clear form
+        setName('')
+        setJob('')
+
+    }
+
+// Returns    
+  return (
+    <form className='add-form' onSubmit={onSubmit}>
+        <div className="form-control">
+            <label>Name</label>
+            <input
+                 type='text'
+                 placeholder='Add Member' 
+                 value={name}
+                 onChange={(e) => setName(e.target.value)}
+            />
+        </div>
+        
+        <div className="form-control">
+            <label>Job Title</label>
+            <input 
+                type='text' 
+                placeholder='Job Title'
+                value={job}
+                onChange={(e) => setJob(e.target.value)}
+            />
+        </div>
+
+        <input type="submit" value="Save Member" className="btn btn-block"/>
+    </form>
+  )
+}
+
+export default AddMember
